b3dcb4e055a020b0f8cb8065841620cbbf25a57c
Maksym Khrystunov <khristunov93@gmail.com>
2016-06-15 20:49:06 +0300

    Initial commit for ChainingComandBundle

---
+++ README.md
@@ -0,0 +1,4 @@
+chain_command
+=============
+
+A Symfony project created on June 15, 2016, 8:05 pm.
---
+++ src/ChainCommandBundle/Command/AbstractChainingCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ChainCommandBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+abstract class AbstractChainingCommand extends ContainerAwareCommand
+{
+}
